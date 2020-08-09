0b5cad58b5aced199ead0422b8e7c3eb5d3da012
Pamylla <pamyllaostermann@gmail.com>
2020-08-09 18:30:00 -0300

    landing page create

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from './pages/landing/index'
+
+import './assets/styles/global.css'
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
